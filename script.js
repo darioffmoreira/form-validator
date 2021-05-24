147ddca692e326adbb38a5fe0d98d51c01b2b998
darioffmoreira <darioffmoreira@gmail.com>
2021-05-24 21:31:38 +0100

    function to check email is valid

--- script.js
+++ script.js
@@ -24,10 +24,14 @@ function showSuccess(input) {
 }
 
 // Check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, `Email is not valid`);
+  }
 }
 
 // Check required fields
@@ -72,4 +76,5 @@ form.addEventListener('submit', function (event) {
   checkRequired([username, email, password, password2]);
   checkLength(username, 3, 15);
   checkLength(password, 6, 25);
+  checkEmail(email);
 });
