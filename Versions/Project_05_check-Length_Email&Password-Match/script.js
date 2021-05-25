a0e8e02d87cb2a2e256e8ce9d0c000c86a877835
darioffmoreira <darioffmoreira@gmail.com>
2021-05-25 08:31:54 +0100

    backup files

---
+++ Versions/Project_05_check-Length_Email&Password-Match/script.js
@@ -0,0 +1,88 @@
+'use strict';
+
+// Pull all the DOM elements that we need
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError(input, message) {
+  //   console.log(input);
+  const formControl = input.parentElement;
+  //   console.log(formControl);
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+// Check email is valid
+function checkEmail(input) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, `Email is not valid`);
+  }
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  // console.log(inputArr);
+  inputArr.forEach(function (input) {
+    // console.log(input.value);
+    // console.log(input.value.trim());
+    // console.log(input.id);
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, `Passwords do not match!`);
+  }
+}
+
+// Get fieldName
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Event listeners
+form.addEventListener('submit', function (event) {
+  // Method called prevent default which prevents it from actually submitting, width this it doesn't flash , it doesn't actually submit the form
+  event.preventDefault();
+
+  // Get the value from username
+  // console.log(username.value);
+
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
+});
